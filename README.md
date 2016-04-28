beb9f8f6a0f9b8b6b530007bb4519bd78c8612d3
Da Moose <ahmed.musawir@hotmail.com>
2016-04-28 08:09:58 +0800

    1st Commit: bootsass setup ...

---
+++ README.md
@@ -0,0 +1,17 @@
+# bootsass-workflow
+This is the workflow for Bootstrap 3.1 and SCSS with Compass. Just clone it and Go !!
+
+
+Compass
+If you are using Compass then you’ll have Ruby installed. With Ruby installed we can use gems, in this case the bootstrap-sass gem. To install:
+###gem install bootstrap-sass
+
+If you have an existing Compass project and want to add bootstrap-sass, run this command:
+###compass install bootstrap -r bootstrap-sass
+
+If you want to start a new Compass project with bootstrap-sass use:
+###compass create my-new-project -r bootstrap-sass --using bootstrap
+
+Bower
+We can also install it with the package manager Bower. To me this option is the best as the other options install a lot of ‘fluff’ that will confuse someone not familiar with Bootstrap. To install with Bower make sure you are in the folder where you want to create your project and run:
+###bower install bootstrap-sass
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include "header.php" ?>
+
+<section class="box">
+   BOURBON TESTING SUCCESS!
+</section>
+		<h1 class="text-center">Welcome To BootySass</h1>
+		<div class="jumbotron">
+			<div class="container">
+				<h1>Hello, Foxy Booty Sass!</h1>
+				<p>Contents ...</p>
+				<p>
+					<a class="btn btn-primary btn-lg">Learn more</a>
+					<a class="btn btn-success btn-lg">Learn more</a>
+					<a class="btn btn-warning btn-lg">Learn more</a>
+					<a class="btn btn-info btn-lg">Learn more</a>
+					<a class="btn btn-danger btn-lg">Learn more</a>
+				</p>
+			</div>
+			<form action="" method="POST" role="form">
+				<legend>Form title</legend>
+			
+				<div class="form-group">
+					<label for="">label</label>
+					<input type="text" class="form-control" id="" placeholder="Input field">
+				</div>
+			
+				
+			
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+
+<?php include "footer.php" ?>
+
+
