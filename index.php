cacb86920d9054b9bd2e5b13ceac662b787e72c7
Da Moose <ahmed.musawir@hotmail.com>
2016-04-28 19:11:21 +0800

    3rd Commit: Footer going on

--- index.php
+++ index.php
@@ -1,46 +1,149 @@
 <?php include "header.php" ?>
 
-<section class="box">
-   BOURBON TESTING SUCCESS!
-</section>
-		<h1 class="text-center">Welcome To BootySass</h1>
-		<div class="jumbotron">
-			<div class="container">
-				<h1>Hello, Foxy Booty Sass!</h1>
-				<p>Contents ...</p>
-				<p>
-					<a class="btn btn-primary btn-lg">Learn more</a>
-					<a class="btn btn-success btn-lg">Learn more</a>
-					<a class="btn btn-warning btn-lg">Learn more</a>
-					<a class="btn btn-info btn-lg">Learn more</a>
-					<a class="btn btn-danger btn-lg">Learn more</a>
-				</p>
-			</div>
-			<form action="" method="POST" role="form">
-				<legend>Form title</legend>
-			
-				<div class="form-group">
-					<label for="">label</label>
-					<input type="text" class="form-control" id="" placeholder="Input field">
-				</div>
-			
-				
-			
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</form>
+<main class="content-wrapper">
+
+<section id="fp-block-1">
+	<article class="block-wrap">
+	
+		<div class="logo text-center">
+			<a href="#">
+				<img class="img-responsive center-block" src="_img/StephaniRobertsLogo.png" alt="">
+			</a>
 		</div>
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td></td>
-				</tr>
-			</tbody>
-		</table>
+		<div class="fp-header-img text-center">
+			<a href="#">
+				<img class="img-responsive center-block" src="_img/front-pg-header-img.png" alt="">
+			</a>
+		</div>		
+
+	</article>
+
+</section>
+
+<section id="fp-block-2">
+
+	<div class="container">
+	   <div class="top-text-block">
+	      <h4>Okay. Sooo I’m not a wardrobe stylist.</h4>
+	      <h4> (FACT: I spend most days in pajama pants.)</h4>
+	      <h4>I AM, however, a <span class="social-media">SOCIAL MEDIA</span> stylist.</h4>
+	   </div>
+	   <!-- .et_pb_text -->
+	   <hr class="block2-hr" >
+	   <div class="bottom-text-block">
+	      <h2 ><span >I’LL HELP YOU <span class="statement" >MAKE A STATEMENT. </span></span></h2>
+	      <h2 ><span class="literally" >(LITERALLY.)</span></h2>
+	   </div>
+	   <!-- .et_pb_text -->
+	</div>
+
+</section>
+
+<section id="fp-block-3">
+
+	<div class="container">
+		<article class="text-block">
+			<h4>I’ve fashioned my dream business around sharing what I know about smart, authentic communication….in between diaper changes, grocery store runs, and imperfectly navigating this thing called “balance.”</h4>
+			<h3>Running a business while running a family is <span class="special">no joke</span>, friends.</h3>
+			<h1>LET’S <span class="special">TALK </span>ABOUT IT.</h1>
+			<h4>Here in my corner of the digi-world, you’ll find stories, tips, and</h4>
+			<h4>inspiration on life, business, and all things communication.</h4>
+			<h3>So let’s start with <span class="special">THAT</span>.</h3>			
+		</article>
+	</div>
+	
+</section>
+
+<section id="fp-block-4">
+
+	<article class="block-wrap container-fluid">
+
+		<div class="col-md-3">
+
+			<article class="social-left">
+
+				<img class="img-responsive" src="_img/fp-blk-4-steph-jump.jpg" alt="">
+
+			</article>
+
+			
+		</div>
+		<div class="col-md-9">
+			<article class="social-right">
+
+				<div class="text-block">
+
+					<h2 class="text-center">SOCIAL MEDIA STYLING TIPS</h2>
+
+					<table>
+						
+						<tbody>
+							<tr>
+								<td><h1>N<sup>o</sup>. 1</h1></td><td><h1 class="purpose">KNOW YOUR <span class="special">PURPOSE</span></h1><p>Every page, post, and paragraph needs a clear point. Be sure you know it BEFORE you start crafting your message.</p></td>
+							</tr>
+							<tr>
+								<td><h1>N<sup>o</sup>. 2</h1></td><td><h1 class="purpose">USE <span class="special">YOUR</span> WORDS</h1><p>(Just like you’d tell your toddler.) Whether you’re talking to your dream clients or your grandma, you’ve got to be yourSELF. Nothing (and no one) else will do.</p></td>
+							</tr>
+							<tr>
+								<td><h1>N<sup>o</sup>. 3</h1></td><td><h1 class="purpose"><span class="special">STYLE</span>'EM UP</h1><p>There are plenty of simple ways to make your words look (+ sound) more like YOU. (That’s where I come in.)</p></td>
+							</tr>												
+						</tbody>
+
+					</table>
+
+					<div class="btn-block">
+						<h4>Click below to learn the SIMPLEST way to attract plenty of dream clients with copy that sounds like YOU :</h4>
+						<a class="btn btn-primary" href="#">SHOW ME</a>						
+					</div>
+					
+				</div>
+
+			</article>		
+		</div>
+
+	</article>
+	
+</section>
+
+
+<section id="footer">
+
+	<div class="container">
+		
+		<article class="footer-social">
+
+			<ul class="social">
+                <li><a href="#"><i class="fa fa-facebook-square"></i> </a></li>
+                <li><a href="#"><i class="fa fa-twitter-square"></i> </a></li>
+                <li><a href="#"><i class="fa fa-google-plus-square"></i> </a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i> </a></li>
+                <li><a href="#"><i class="fa fa-pinterest-square"></i> </a></li>
+            </ul>   		
+			
+		</article>
+		<nav class="footer-nav">
+			<ul class="nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+
+		<p>© 2013 - 2016 Nikki Elledge Brown, LLC | All Rights Reserved | LEGAL</p>
+
+	</div>
+	
+</section>
+
+
+<section id="footer-call-out">
+	
+</section>
+	
+</main>
+
 
 <?php include "footer.php" ?>
 
